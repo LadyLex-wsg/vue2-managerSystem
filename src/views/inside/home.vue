65af988b42bd2f6549554829e4d012ac7a26153f
LadyLex_wsg <2468270875@qq.com>
2022-09-24 14:10:49 +0800

    首页v0.1

--- src/views/inside/home.vue
+++ src/views/inside/home.vue
@@ -1,15 +1,37 @@
 <template>
-    <div>
-        首页
+    <div id="app">
+        <el-container style="height: 100%">
+            <el-aside width="200px"> </el-aside>
+
+            <el-container>
+                <el-header height="80px"> </el-header>
+
+                <el-main>
+                    <router-view />
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+::-webkit-scrollbar {
+    display: none;
+}
+#app {
+    .el-aside {
+        overflow-x: hidden;
+        overflow-y: auto;
+        background-color: #69adf1;
+        scrollbar-width: none;
+        -ms-overflow-style: none;
+    }
+    .el-header {
+        background-color: #d3dce6;
+    }
+}
 </style>
